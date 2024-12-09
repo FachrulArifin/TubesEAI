31815b7b1af7a699b6be0cabc83ce6038a3a4fc4
Arif <fachrul.arifin29@gmail.com>
2024-12-09 16:48:55 +0700

    Penambahan Login dan register page

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [OrderController::class, 'index']);
+
+// Routes Login Controll
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+
+// Routes Order
+
 Route::post('/checkout', [OrderController::class, 'checkout']);
 Route::get('/invoice/{id}', [OrderController::class, 'invoice']);
